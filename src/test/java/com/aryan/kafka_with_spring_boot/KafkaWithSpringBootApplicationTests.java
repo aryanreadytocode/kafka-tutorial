984bc65337179d10fa83ef81368ec501ece10198
Raj Aryan <arpittaryan2k12@gmail.com>
2024-10-10 21:07:03 +0530

    first commit

---
+++ src/test/java/com/aryan/kafka_with_spring_boot/KafkaWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.kafka_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
